4b3b4a96f736820dedf8767813cd97c9f799645c
Jean Roström <jean@rostrom.se>
2020-12-01 20:31:25 +0100

    Add day 2

---
+++ day2/src/main.rs
@@ -0,0 +1,19 @@
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::fs::File;
+use std::path::Path;
+
+fn lines_from_file<P>(filename: P) -> Vec<String>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+fn main() {
+    println!("Hello, world!");
+}
